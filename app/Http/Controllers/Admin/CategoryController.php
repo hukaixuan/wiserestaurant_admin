d9820f4a61ac21a4a3451f6242bfac657a7b212e
kacey <1782964880@qq.com>
2016-09-07 09:32:59 +0800

    区分大小写

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use DB;
-use App\category;
+use App\Category;
 
 class CategoryController extends Controller
 {
